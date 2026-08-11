021cf8c9abbaae2d62ea30ea03751f6babae5219
Phyllis Tenkorang <phyllist8a@gmail.com>
2026-08-11 15:15:43 +0200

    Shorten PSA threshold legend title

--- analysis/03_probabilistic_sensitivity_analysis.R
+++ analysis/03_probabilistic_sensitivity_analysis.R
@@ -255,7 +255,7 @@ publication_plot <- ggplot(
       c("dotted", "solid", "dashed"),
       levels(wtp_lines$threshold)
     ),
-    name = "Cost-effectiveness thresholds (THB per birth averted):",
+    name = "Cost-effectiveness thresholds (THB):",
     guide = guide_legend(
       nrow = 3,
       byrow = TRUE,
